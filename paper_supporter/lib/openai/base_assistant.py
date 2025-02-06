9a72d1bc86f16b02a50280abf846c997259d8e8e
masashi <masashi.lateolabrax@gmail.com>
2025-02-06 21:31:45 +0900

    mod: remove on_tool_call_delta()

--- paper_supporter/lib/openai/base_assistant.py
+++ paper_supporter/lib/openai/base_assistant.py
@@ -39,16 +39,6 @@ class EventHandler(AssistantEventHandler):
         """
         print(f"\nassistant > {tool_call.type}\n", flush=True)
 
-    def on_tool_call_delta(self, delta, snapshot):
-        if delta.type == 'code_interpreter':
-            if delta.code_interpreter.input:
-                print(delta.code_interpreter.input, end="", flush=True)
-            if delta.code_interpreter.outputs:
-                print(f"\n\noutput >", flush=True)
-                for output in delta.code_interpreter.outputs:
-                    if output.type == "logs":
-                        print(f"\n{output.logs}", flush=True)
-
 
 class BaseAssistant:
     """
