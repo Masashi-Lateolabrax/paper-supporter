599723f8b47ac62ee83219042d56afc6c1d6c90e
masashi <masashi.lateolabrax@gmail.com>
2025-02-06 15:38:22 +0900

    add: Change BaseFileAssistant's parent from BaseAssistant to BaseIntelligenceAssistant

--- paper_supporter/lib/openai/base_file_assistant.py
+++ paper_supporter/lib/openai/base_file_assistant.py
@@ -1,17 +1,12 @@
 from openai import NOT_GIVEN
-from openai.types.beta import vector_store_create_params, assistant_update_params
-from openai.types.beta.assistant_update_params import ToolResourcesFileSearch
-from openai.types.beta.file_search_tool_param import FileSearchToolParam, FileSearch
-from openai.types.file_object import FileObject
+from openai.types.beta import vector_store_create_params
 
-from .base_assistant import CLIENT, BaseAssistant
+from .base_assistant import CLIENT
+from .base_intelligence_assistant import BaseIntelligenceAssistant
 
 
-class BaseFileAssistant(BaseAssistant):
+class BaseFileAssistant(BaseIntelligenceAssistant):
     def __init__(self, model: str, instruction: str = NOT_GIVEN):
-        super().__init__(model)
-        self.files: list[FileObject] = []
-
         self.vector_store = CLIENT.beta.vector_stores.create(
             expires_after=vector_store_create_params.ExpiresAfter(
                 anchor="last_active_at",
@@ -19,37 +14,13 @@ class BaseFileAssistant(BaseAssistant):
             ),
         )
 
-        self._modify(
-            tools=[
-                FileSearchToolParam(
-                    type="file_search",
-                    file_search=FileSearch()
-                )
-            ],
-            tool_resources=assistant_update_params.ToolResources(
-                file_search=ToolResourcesFileSearch(
-                    vector_store_ids=[self.vector_store.id]
-                )
-            ),
-            instructions=instruction
-        )
+        super().__init__(model, storage_id=self.vector_store.id)
+        self.set_instructions(instruction)
 
     def __del__(self):
+        for f in self.get_files():
+            self.remove_file_from_storage(f.id)
         CLIENT.beta.vector_stores.delete(
             vector_store_id=self.vector_store.id
         )
-        for f in self.files:
-            CLIENT.files.delete(f.id)
         super().__del__()
-
-    def add_file(self, file_object):
-        self.files.append(
-            CLIENT.files.create(
-                file=file_object,
-                purpose="assistants"
-            )
-        )
-        CLIENT.beta.vector_stores.files.create(
-            vector_store_id=self.vector_store.id,
-            file_id=self.files[-1].id
-        )
