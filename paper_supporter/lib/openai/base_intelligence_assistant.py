42f2a9746b92dd529ee9fdefa80a555c2efec7cd
masashi <masashi.lateolabrax@gmail.com>
2025-02-06 23:12:41 +0900

    mod: Write BaseIntelligenceAssistant Documents

--- paper_supporter/lib/openai/base_intelligence_assistant.py
+++ paper_supporter/lib/openai/base_intelligence_assistant.py
@@ -7,7 +7,18 @@ from .base_assistant import BaseAssistant, CLIENT
 
 
 class BaseIntelligenceAssistant(BaseAssistant):
+    """
+    BaseIntelligenceAssistant class to interact with the OpenAI assistant with additional file search capabilities.
+    """
+
     def __init__(self, model: str, storage_id: str = None):
+        """
+        Initialize the BaseIntelligenceAssistant with a specific model and optional storage ID.
+
+        :param model: The model to use for the assistant.
+        :param storage_id: The storage ID for the vector store. If None, a new vector store is created.
+        """
+
         super().__init__(model)
 
         if storage_id is None:
@@ -32,9 +43,21 @@ class BaseIntelligenceAssistant(BaseAssistant):
         )
 
     def get_storage_id(self):
+        """
+        Get the storage ID of the vector store.
+
+        :return: The storage ID.
+        """
+
         return self.storage.id
 
     def add_file(self, file_object):
+        """
+        Add a file to the vector store.
+
+        :param file_object: The file object to add.
+        """
+
         file = CLIENT.files.create(
             file=file_object,
             purpose="assistants"
@@ -45,11 +68,23 @@ class BaseIntelligenceAssistant(BaseAssistant):
         )
 
     def get_files(self) -> list[VectorStoreFile]:
+        """
+        Get the list of files in the vector store.
+
+        :return: A list of VectorStoreFile objects.
+        """
+
         return CLIENT.beta.vector_stores.files.list(
             vector_store_id=self.storage.id
         )
 
     def remove_file_from_vector_store(self, id_):
+        """
+        Remove a file from the vector store by its ID.
+
+        :param id_: The ID of the file to remove.
+        """
+
         CLIENT.beta.vector_stores.files.delete(
             vector_store_id=self.storage.id,
             file_id=id_
@@ -57,9 +92,22 @@ class BaseIntelligenceAssistant(BaseAssistant):
 
     @staticmethod
     def remove_file_from_storage(id_):
+        """
+        Remove a file from the storage by its ID.
+
+        :param id_: The ID of the file to remove.
+        """
+
         CLIENT.files.delete(id_)
 
     def remove_file(self, file: VectorStoreFile, remove_file_on_storage=True):
+        """
+        Remove a file from the vector store and optionally from the storage.
+
+        :param file: The VectorStoreFile object to remove.
+        :param remove_file_on_storage: Whether to remove the file from the storage as well.
+        """
+
         self.remove_file_from_vector_store(file.id)
         if remove_file_on_storage:
             BaseIntelligenceAssistant.remove_file_from_storage(file.id)
